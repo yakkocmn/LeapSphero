022e7362e0e9e5a0cb28dc6ee9fd0bd9bcb29187
Chris Newsome <legoclonetrooperdude@gmail.com>
2016-02-02 08:30:44 -0800

    testing leap

---
+++ task.js
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+
+var leapjs = require('leapjs');
+
+var controller = new leapjs.Controller();
+
+controller.on('connect', function() {
+  console.log("Successfully connected.");
+});
+
+controller.on('deviceConnected', function() {
+  console.log("A Leap device has been connected.");
+});
+
+controller.on('deviceDisconnected', function() {
+  console.log("A Leap device has been disconnected.");
+});
+
+controller.connect();
